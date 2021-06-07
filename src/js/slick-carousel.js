8dc1abdf1c2ffafb5c40386ad940a4cdfe12a7b6
NataliaBulanova <nbulanova91@gmail.com>
2021-06-07 12:11:55 +0300

    Create section reviews

---
+++ src/js/slick-carousel.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+//   $('.gallery__list').slick({
+//     arrows: true,
+//     dots: true,
+//     autoplay: true,
+//     autoplaySpeed: 250,
+//     centerMode: true,
+//     variableWidth: true,
+//   });
+// });
+
+$('.review__list').slick({
+  arrows: false,
+  dots: true,
+});
