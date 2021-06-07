1e82473f99b9d954f2829ac30e573bf0ac4f06d0
NataliaBulanova <nbulanova91@gmail.com>
2021-06-07 18:55:26 +0300

    delete commented part in slick.js

--- src/js/slick-carousel.js
+++ src/js/slick-carousel.js
@@ -1,15 +1,8 @@
-// $(document).ready(function () {
-//   $('.gallery__list').slick({
-//     arrows: true,
-//     dots: true,
-//     autoplay: true,
-//     autoplaySpeed: 250,
-//     centerMode: true,
-//     variableWidth: true,
-//   });
-// });
-
-$('.review__list').slick({
-  arrows: false,
-  dots: true,
+$(document).ready(function () {
+  $('.review__list').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
