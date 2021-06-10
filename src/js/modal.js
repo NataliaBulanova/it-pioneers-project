a18221550d12b9a22ce3b93194a80873f64fb65e
Viacheslav2296 <menzulinvaceslav@gmail.com>
2021-06-10 22:21:35 +0300

    Изменения, которые я не делал

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,6 @@
 (() => {
     const refs = {
-        open_ModalBtn: document.querySelector('[data-1-modal-open]'),
+        open_ModalBtn: document.querySelector('[data1-modal-open]'),
         openModalBtn: document.querySelector('[data-modal-open]'),
         closeModalBtn: document.querySelector('[data-modal-close]'),
         modal: document.querySelector('[data-modal]'),
@@ -10,26 +10,8 @@
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
 
-
     function toggleModal() {
         document.body.classList.toggle("modal-open");
         refs.modal.classList.toggle('is-hidden');
     }
-
 })();
-
-{
-    const refsMap = {
-        openModalBtn: document.querySelector('[data-modal-open-map]'),
-        closeModalBtn: document.querySelector('[data-modal-close-map]'),
-        modal: document.querySelector('[data-modal-map]'),
-    };
-
-    refsMap.openModalBtn.addEventListener('click', toggleModal);
-    refsMap.closeModalBtn.addEventListener('click', toggleModal);
-
-    function toggleModal() {
-        document.body.classList.toggle("modal-open-map");
-        refsMap.modal.classList.toggle('is-hidden-map');
-    }
-}
